821217286fb549e63042ba07f4375e7cea892d9e
TrelohnaT <48327399+TrelohnaT@users.noreply.github.com>
2020-08-10 13:26:35 +0200

    a

---
+++ index_script.js
@@ -0,0 +1,18 @@
+/*
+
+** Autor: Václav Doleček
+
+ */
+
+
+function mouse_in()
+{
+    document.getElementById("menu_part_picture").style.width = "100%";
+    document.getElementById("menu_part_picture").style.height = "650px";
+}
+
+function mouse_out()
+{
+    document.getElementById("menu_part_picture").style.width = "42%";
+    document.getElementById("menu_part_picture").style.height = "200px";
+}
