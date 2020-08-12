4e97e747181b5dda2cd6e32715270a5e5aac4fed
TrelohnaT <48327399+TrelohnaT@users.noreply.github.com>
2020-08-12 11:39:40 +0200

    main page - verze 0.5
    Vytvoření karty pro hru Eager Miner, česání HTML, upravy CSS.

--- index_script.js
+++ index_script.js
@@ -5,14 +5,36 @@
  */
 
 
-function mouse_in()
+async function mouse_in(block)
 {
-    document.getElementById("menu_part_picture").style.width = "100%";
-    document.getElementById("menu_part_picture").style.height = "650px";
+    if(block == 1)
+    {
+        /*zvětšení celého bloku aby se tam vešel obrázek*/
+        document.getElementById("mine").style.height = "650px";
+        /*zvětšení obrázku na šířku */
+        document.getElementById("menu_part_picture").style.height = "650px";
+        /*zvětšení obrázku na výšku */
+        document.getElementById("menu_part_picture").style.width = "650px";
+        /*zviditelnění popisku 1*/
+        document.getElementById("sample_text_miner_p1").style.display = "block";
+        /*zviditelnění popisku 2*/
+        document.getElementById("sample_text_miner_p2").style.display = "block";
+    }
 }
 
-function mouse_out()
+function mouse_out(block)
 {
-    document.getElementById("menu_part_picture").style.width = "42%";
-    document.getElementById("menu_part_picture").style.height = "200px";
+    if(block == 1)
+    {
+        /*zmenšení obrázku na výšku */
+        document.getElementById("menu_part_picture").style.width = "42%";
+        /*zmenšení obrázku na šířku */
+        document.getElementById("menu_part_picture").style.height = "200px";
+        /*z neviditelnění popisku 1*/
+        document.getElementById("sample_text_miner_p1").style.display = "none";
+        /*z neviditelnění popisku 2*/
+        document.getElementById("sample_text_miner_p2").style.display = "none";
+        /*zmenšení bloku */
+        document.getElementById("mine").style.height = "200px";
+    }
 }
